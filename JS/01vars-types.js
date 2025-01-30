26a1450b3fb3131f1c9a1ed9330db2f3b7a072a0
Sergei <s.leushkin1509@gmail.com>
2025-01-30 12:25:29 +0200

    practice

---
+++ JS/01vars-types.js
@@ -0,0 +1,19 @@
+const myName = "Serhii";
+console.log(myName);
+const products = "bananas";
+const pricePerItem = 10;
+console.log(products, pricePerItem);
+
+const speed = 120;
+const distance = 617.54;
+const isAdmin = true;
+// const congratulation = "Привіт вітаємо на нашому сайті!"
+// alert(congratulation)
+
+const yourName = "Яке твоє ім'я";
+const userName = prompt(yourName)
+console.log(userName);
+console.log(typeof userName);
+
+const shouldRenew = confirm("Чи хочете продовжити підписку?")
+console.log(shouldRenew);
